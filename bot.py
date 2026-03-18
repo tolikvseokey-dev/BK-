fd755869084905f164de0addc517ffabfe6d3118
tolikvseokey-dev <manjak.tolik@gmail.com>
2026-03-18 22:21:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,20 @@
+import os
+from dotenv import load_dotenv
+import telebot
 
+# Загружаем переменные из .env
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Бот запущен 🚀")
+
+
+print("Бот запущен...")
+bot.infinity_polling()
